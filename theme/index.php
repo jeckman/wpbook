597b4dafffe7b40a03600309a04d72882e1432d7
John Eckman <eckman.john@gmail.com>
2010-08-14 19:30:45 +0000

    cleanup fbml for app tab
    git-svn-id: http://plugins.svn.wordpress.org/wpbook/trunk@275917 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- theme/index.php
+++ theme/index.php
@@ -6,11 +6,46 @@ if(isset($_GET['app_tab'])) { // this is an app tab
   ?>
   <fb:fbml>
   <style type="text/css">
-  <?php 
-    // online inline styles allowed in fbml, so echo 'em out 
-    echo file_get_contents(WP_PLUGIN_DIR . '/wpbook/theme/default/style.css');
-  ?>
-    </style>
+  .box_head{
+    padding: 5px 0 0 0;
+  }
+  .wpbook_box_header{
+    padding: 1px 6px 0px 8px; 
+    border-top: solid 1px #3B5998; 
+    background: #d8dfea;
+  }
+  .wpbook_external_post {
+    margin-top: 5px;
+    margin-bottom: 10px;
+    padding-left: 5px;
+    height: 25px;
+  }
+  .wpbook_external_post a { 
+    background-color: white;
+    background-repeat: repeat-y;
+    background-attachment: scroll;
+    background-position: right center;
+    border: 1px solid #7F93BC;
+    line-height: 11px;
+    padding-top: 0pt;
+    padding-bottom: 1px;
+    padding-left: 5px;
+    padding-right: 5px;
+  } 
+
+  .wpbook_external_post a:hover {
+    color: #ffffff;
+    border: 1px solid #3B5998;
+    text-decoration: none;
+    background-color: #3b5998;
+    background-attachment: scroll;
+    background-position: right center;
+  }  
+
+  .wpbook_header{
+    margin-bottom: 5px;
+  }
+  </style>
   <div id="content">
   <?php 	
   have_posts();
@@ -31,16 +66,62 @@ if(isset($_GET['app_tab'])) { // this is an app tab
     if(($enable_share == "true" || $enable_external_link == "true") && ($links_position == "top")) { 
       echo '<p>';
       if($enable_share == "true"){
-        echo '<fb:share-button class="url" href="'. urlencode(get_permalink()) .'" />';
+        echo '<fb:share-button class="url" href="'. get_permalink() .'" />';
       } // end enable_share = true 
       if($enable_external_link == "true"){
         ?><span class="uiButton uiButtonMedium"><a class="uiButtonText" href="<?php echo get_external_post_url(get_permalink()); ?>" title="View this post outside Facebook at <?php bloginfo('name'); ?>">View post on <?php bloginfo('name'); ?></a></span><?php
       }
         echo '</p>';
     } // end if for enable share, external, top
-            
-      the_content();
-            
+      
+      $wpbook_allowed = array('address' => array(),
+                              'a' => array('class' => array (),'href' => array (),'id' => array (),'title' => array (),'rel' => array (),'rev' => array (),'name' => array (),'target' => array()),
+                              'abbr' => array('class' => array (),'title' => array ()),
+                              'acronym' => array('title' => array ()),
+                              'article' => array('align' => array (),'class' => array (),'dir' => array (),'lang' => array(),'style' => array (),'xml:lang' => array(),),
+                              'b' => array(),
+                              'blockquote' => array('id' => array (),'cite' => array (),'class' => array(),'lang' => array(),'xml:lang' => array()),
+                              'br' => array ('class' => array ()),
+                              'caption' => array('align' => array (),'class' => array ()),
+                              'code' => array ('style' => array()),
+                              'del' => array('datetime' => array ()),
+                              'dd' => array(),
+                              'div' => array('align' => array (),'class' => array (),'dir' => array (),'lang' => array(),'style' => array (),'xml:lang' => array()),
+                              'dl' => array(),
+                              'dt' => array(),
+                              'em' => array(),
+                              'h1' => array('align' => array (),'class' => array (),'id'    => array (),'style' => array ()),
+                              'h2' => array('align' => array (),'class' => array (),'id'    => array (),'style' => array ()),
+                              'h3' => array('align' => array (),'class' => array (),'id'    => array (),'style' => array ()),
+                              'h4' => array('align' => array (),'class' => array (),'id'    => array (),'style' => array ()),
+                              'h5' => array('align' => array (),'class' => array (),'id'    => array (),'style' => array ()),
+                              'h6' => array('align' => array (),'class' => array (),'id'    => array (),'style' => array ()),
+                              'i' => array(),
+                              'img' => array('alt' => array (),'align' => array (),'border' => array (),'class' => array (),'height' => array (),'hspace' => array (),'longdesc' => array (),'vspace' => array (),'src' => array (),'style' => array (),'width' => array ()),
+                              'li' => array ('align' => array (),'class' => array ()),
+                              'p' => array('class' => array (),'align' => array (),'dir' => array(),'lang' => array(),'style' => array (),'xml:lang' => array()),
+                              'pre' => array('style' => array(),'width' => array ()),
+                              'span' => array ('class' => array (),'dir' => array (),'align' => array (),'lang' => array (),'style' => array (),'title' => array (),'xml:lang' => array()),
+                              'strike' => array(),'strong' => array(),'sub' => array(),
+                              'table' => array('align' => array (),'bgcolor' => array (),'border' => array (),'cellpadding' => array (),'cellspacing' => array (),'class' => array (),'dir' => array(),'id' => array(),'rules' => array (),'style' => array (),'summary' => array (),'width' => array ()),
+                              'tbody' => array('align' => array (),'char' => array (),'charoff' => array (),'valign' => array ()),
+                              'td' => array('abbr' => array (),'align' => array (),'axis' => array (),'bgcolor' => array (),
+                                            'char' => array (),'charoff' => array (),'class' => array (),'colspan' => array (),'dir' => array(),'headers' => array (),'height' => array (),'nowrap' => array (),'rowspan' => array (),'scope' => array (),'style' => array (),'valign' => array (),'width' => array ()),
+                              'title' => array(),
+                              'tr' => array('align' => array (),'bgcolor' => array (),'char' => array (),'charoff' => array (),'class' => array (),
+                                            'style' => array (),'valign' => array ()),
+                              'u' => array(),
+                              'ul' => array ('class' => array (),'style' => array (),'type' => array ()),
+                              'ol' => array ('class' => array (),'start' => array (),'style' => array (),'type' => array ()),
+                              'var' => array ());
+          
+      $content = get_the_content();
+      $content = apply_filters('the_content', $content);
+      $content = str_replace(']]>', ']]&gt;', $content);
+
+      echo wp_kses($content,$wpbook_allowed);
+        
+          //the_content();    
       // echo custom footer
       if(($show_custom_header_footer == "footer") || ($show_custom_header_footer == "both")){	
         echo('<div id="custom_footer">'.custom_header_footer($custom_footer,$timestamp_date_format,$timestamp_time_format) .'</div>');
@@ -50,7 +131,7 @@ if(isset($_GET['app_tab'])) { // this is an app tab
       if(($enable_share == "true" || $enable_external_link == "true") && ($links_position == "bottom")) { 
         echo '<p>';
         if($enable_share == "true"){
-          echo '<fb:share-button class="url" href="'. urlencode(get_permalink()) .'" />';
+          echo '<fb:share-button class="url" href="'. get_permalink() .'" />';
         } // end enable_share = true 
         if($enable_external_link == "true"){
           ?><span class="uiButton uiButtonMedium"><a class="uiButtonText" href="<?php echo get_external_post_url(get_permalink()); ?>" title="View this post outside Facebook at <?php bloginfo('name'); ?>">View post on <?php bloginfo('name'); ?></a></span><?php
