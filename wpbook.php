de924f642d7541a3a44a4189e19e34033830ea56
John Eckman <eckman.john@gmail.com>
2010-08-17 18:02:09 +0000

    tagging 2.0.5 for release
    git-svn-id: http://plugins.svn.wordpress.org/wpbook/trunk@277402 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpbook.php
+++ wpbook.php
@@ -2,11 +2,11 @@
 /*
 Plugin Name: WPBook
 Plugin URI: http://www.openparenthesis.org/code/wp
-Date: 2010, August 16th
+Date: 2010, August 17th
 Description: Plugin to embed Wordpress Blog into Facebook Canvas using the Facebook Platform. 
 Author: John Eckman
 Author URI: http://johneckman.com
-Version: 2.0.4
+Version: 2.0.5
 */
   
 /*
