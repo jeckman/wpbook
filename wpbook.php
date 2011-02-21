7c5e81a071dd67d09a3a7c8e6c07bc057aaf2651
John Eckman <eckman.john@gmail.com>
2011-02-21 14:55:32 +0000

    updating is_facebook to check for signed_request and adding supercache constant
    git-svn-id: http://plugins.svn.wordpress.org/wpbook/trunk@348710 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpbook.php
+++ wpbook.php
@@ -850,9 +850,15 @@ function check_facebook() {
 	if (!isset($_SERVER["HTTP_USER_AGENT"])) {
 		return false;
 	}
-	if (isset($_GET['fb_sig_in_iframe']) || isset($_GET['fb_force_mode'])) {  
-		return true;
+	if ((isset($_GET['fb_sig_in_iframe']) || isset($_GET['fb_force_mode'])) {  
+		defined('DONOTCACHEPAGE') or define('DONOTCACHEPAGE', 'true'); 
+    return true;
 	}
+  /* need to check for signed_request to trap new iframes */ 
+  if ((isset($_POST['signed_request']) || isset($_GET['signed_request'])) {
+    defined('DONOTCACHEPAGE') or define('DONOTCACHEPAGE', 'true'); 
+      return true; 
+  }
 	return false;
 }
 
