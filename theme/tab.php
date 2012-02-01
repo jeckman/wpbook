00fd18ee5ea13cd2d1859928f604f0cc55520cf4
John Eckman <eckman.john@gmail.com>
2012-02-01 19:27:40 +0000

    updating tab to show 5 recent posts even when home is different
    git-svn-id: http://plugins.svn.wordpress.org/wpbook/trunk@498733 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- theme/tab.php
+++ theme/tab.php
@@ -5,9 +5,52 @@ echo '<html xmlns="http://www.w3.org/1999/xhtml" ';
 echo 'xmlns:fb="http://www.facebook.com/2008/fbml">';
 echo '<head>';
 echo '<title>'. get_bloginfo('name').' :: Facebook Blog Application</title>';
-if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
 wp_head();
-echo '<link rel="stylesheet" href="'. WP_PLUGIN_URL .'/wpbook/theme/default/style.css" type="text/css" media="screen" />';
+echo '<link rel="stylesheet" href="'. WP_PLUGIN_URL .'/wpbook/theme/default/style.css" 
+type="text/css" media="screen" />';
+?>
+<style type="text/css">
+.box_head{ padding: 5px 0 0 0;}
+.wpbook_box_header{
+    padding: 1px 6px 0px 8px; 
+    border-top: solid 1px #3B5998; 
+    background: #d8dfea;
+}
+.wpbook_external_post {
+    margin-top: 5px;
+    margin-bottom: 10px;
+    padding-left: 5px;
+    height: 25px;
+}
+.wpbook_external_post a { 
+    background-color: white;
+    background-repeat: repeat-y;
+    background-attachment: scroll;
+    background-position: right center;
+    border: 1px solid #7F93BC;
+    line-height: 11px;
+    padding-top: 0pt;
+    padding-bottom: 1px;
+    padding-left: 5px;
+    padding-right: 5px;
+} 
+
+.wpbook_external_post a:hover {
+    color: #ffffff;
+    border: 1px solid #3B5998;
+    text-decoration: none;
+    background-color: #3b5998;
+    background-attachment: scroll;
+    background-position: right center;
+}  
+
+  .wpbook_header{
+    margin-bottom: 5px;
+}
+
+</style>
+
+<?php
 echo '<BASE TARGET="_top">';	
 echo '</head>';
 echo '<body>';
@@ -22,108 +65,89 @@ if((!empty($data["user_id"])) && ($invite_friends == "true")) {
 echo '<h3><a href="'. $proto .'://apps.facebook.com/'. $app_url .'/" target="_top">'. get_bloginfo('name') .'</a></h3>';
 
 echo '<div id="content">';	
-if (have_posts()) { 
-	while (have_posts()) { 
-		the_post();
-		if (is_single() || $wp_query->is_single || $wp_query->is_singular) {
-			previous_post_link('&laquo; Previous Post: %link <br />',
-								'%title',FALSE,'');
-			next_post_link('Next Post: %link &raquo;<br />',
-								'%title',FALSE,'');
-		} //end if single  
-		echo '<div class="box_head clearfix" id="post-'. get_the_ID() .'">';
-		echo '<h3 class="wpbook_box_header">';
-		if($show_date_title == "true"){
-			the_time($timestamp_date_format); 
-			echo(" - ");
-		}
-		echo '<a href="'. get_permalink() .'" target="_top">';
-		echo the_title() .'</a></h3>'; 
-		if(($show_custom_header_footer == "header") || ($show_custom_header_footer == "both")){
-			echo( '<div id="custom_header">'.custom_header_footer($custom_header,$timestamp_date_format,$timestamp_time_format) .'</div>');
-		} // end if for showing customer header
+
+global $post;
+$args = array('numberposts' => 5);
+$myposts = get_posts( $args );
+foreach( $myposts as $post ) {
+	setup_postdata($post); 
+	//the_content();
+	echo '<div class="box_head clearfix" id="post-'. get_the_ID() .'">';
+	echo '<h3 class="wpbook_box_header">';
+	if($show_date_title == "true"){
+		echo get_the_time($timestamp_date_format) ." - ";
+	}
+	echo '<a href="'. get_external_post_url(get_permalink()) .'" target="_top">'. get_the_title() .'</a></h3>'; 
+	if(($show_custom_header_footer == "header") || ($show_custom_header_footer == "both")){
+		echo( '<div id="custom_header">'.custom_header_footer($custom_header,$timestamp_date_format,$timestamp_time_format) .'</div>');
+	} // end if for showing customer header
   
-		if(($enable_share == "true" || $enable_external_link == "true") && ($links_position == "top")) { 
-			echo '<p>';
-			if($enable_share == "true"){
-				echo '<span class="wpbook_share_button">';
-				echo '<a onclick="window.open(\'http://www.facebook.com/sharer.php?s=100&amp;p[title]=';
-				echo urlencode(get_the_title());
-				echo '&amp;p[summary]=';
-				echo urlencode((wp_filter_nohtml_kses(apply_filters('the_content',get_the_excerpt()))));
-				if((function_exists('has_post_thumbnail')) && (has_post_thumbnail())) {
-					$my_thumb_id = get_post_thumbnail_id();
-					$my_thumb_array = wp_get_attachment_image_src($my_thumb_id);
-					$my_image = $my_thumb_array[0]; // this should be the url                
-					echo '&amp;p[images][0]=';
-					echo urlencode($my_image);
-				}
-				echo '&amp;p[url]=';
-				echo urlencode(get_permalink());
-				echo "','sharer','toolbar=0,status=0,width=626,height=436'); return false;\""; 
-				echo ' class="share" title="Send this to friends or post it on your profile.">Share This Post</a>';
-				echo '</span>';
-			} // end if for enable_share 
-			if($enable_external_link == "true"){ 
-				echo '<span class="wpbook_external_post"><a href="'. get_external_post_url(get_permalink()) .'" title="View this post outside Facebook at '. get_bloginfo('name') .'">View post on '. get_bloginfo('name') .'</a></span>';
-			} // end if for enable external_link
-			echo '</p>';
-		} // end links_position _top
-		the_content();
-		// echo custom footer
-		if(($show_custom_header_footer == "footer") || ($show_custom_header_footer == "both")){	
-			echo('<div id="custom_footer">'.custom_header_footer($custom_footer,$timestamp_date_format,$timestamp_time_format) .'</div>');
-		} // endif for footer 
-            
-		// get share link 
-		if(($enable_share == "true" || $enable_external_link == "true") && ($links_position == "bottom")) { 
-			echo '<p>';
-			if($enable_share == "true"){
-				echo '<span class="wpbook_share_button">';
-				echo '<a onclick="window.open(\'http://www.facebook.com/sharer.php?s=100&amp;p[title]=';
-				echo urlencode(get_the_title());
-				echo '&amp;p[summary]=';
-				echo urlencode((wp_filter_nohtml_kses(apply_filters('the_content',get_the_excerpt()))));
-				if((function_exists('has_post_thumbnail')) && (has_post_thumbnail())) {
-					$my_thumb_id = get_post_thumbnail_id();
-					$my_thumb_array = wp_get_attachment_image_src($my_thumb_id);
-					$my_image = $my_thumb_array[0]; // this should be the url                
-					echo '&amp;p[images][0]=';
-					echo urlencode($my_image);
-				}
-				echo '&amp;p[url]=';
-				echo urlencode(get_permalink());
-				echo "','sharer','toolbar=0,status=0,width=626,height=436'); return false;\""; 
-				echo ' class="share" title="Send this to friends or post it on your profile.">Share This Post</a>';
-				echo '</span>';
-			} // end if for enable_share              
-			if($enable_external_link == "true"){
-				echo '<span class="wpbook_external_post"><a href="'. get_external_post_url(get_permalink()) .'"';
-				echo ' title="View this post outside Facebook at '. get_bloginfo('name') .'">View post on '. get_bloginfo('name') .'</a></span>';
+	if(($enable_share == "true" || $enable_external_link == "true") && ($links_position == "top")) { 
+		echo '<p>';
+		if($enable_share == "true"){
+			echo '<span class="wpbook_share_button">';
+			echo '<a onclick="window.open(\'http://www.facebook.com/sharer.php?s=100&amp;p[title]=';
+			echo urlencode(get_the_title());
+			echo '&amp;p[summary]=';
+			echo urlencode((wp_filter_nohtml_kses(apply_filters('the_content',get_the_excerpt()))));
+			if((function_exists('has_post_thumbnail')) && (has_post_thumbnail())) {
+				$my_thumb_id = get_post_thumbnail_id();
+				$my_thumb_array = wp_get_attachment_image_src($my_thumb_id);
+				$my_image = $my_thumb_array[0]; // this should be the url                
+				echo '&amp;p[images][0]=';
+				echo urlencode($my_image);
 			}
-			echo '</p>';
-		} // end if for enable share, external, bottom
-		echo '</div>';	
-                  
-		comments_template();           
-	} // end while have posts
-                
-	echo '<h3 class="wpbook_box_header">More Posts</h3>'; 
-	echo '<p>';
-	$wpbook_next_page = get_next_posts_link();
-	$wpbook_prev_page = get_previous_posts_link();
-	if ($wpbook_prev_page)
-		echo $wpbook_prev_page;
-	if ($wpbook_prev_page && $wpbook_next_page) 
-		echo  ' | ';
-	if ($wpbook_next_page)
-		echo $wpbook_next_page;
-	echo '</p>'; 
-} // end if have posts	
+			echo '&amp;p[url]=';
+			echo urlencode(get_permalink());
+			echo "','sharer','toolbar=0,status=0,width=626,height=436'); return false;\""; 
+			echo ' class="share" title="Send this to friends or post it on your profile.">Share This Post</a>';
+			echo '</span>';
+		} // end if for enable_share 
+		if($enable_external_link == "true"){ 
+			echo '<span class="wpbook_external_post"><a href="'. get_external_post_url(get_permalink()) .'" title="View this post outside Facebook at '. get_bloginfo('name') .'">View post on '. get_bloginfo('name') .'</a></span>';
+		} // end if for enable external_link
+		echo '</p>';
+	} // end links_position _top
+	the_content();
+	// echo custom footer
+	if(($show_custom_header_footer == "footer") || ($show_custom_header_footer == "both")){	
+		echo('<div id="custom_footer">'.custom_header_footer($custom_footer,$timestamp_date_format,$timestamp_time_format) .'</div>');
+	} // endif for footer 
+            
+	// get share link 
+	if(($enable_share == "true" || $enable_external_link == "true") && ($links_position == "bottom")) { 
+		echo '<p>';
+		if($enable_share == "true"){
+			echo '<span class="wpbook_share_button">';
+			echo '<a onclick="window.open(\'http://www.facebook.com/sharer.php?s=100&amp;p[title]=';
+			echo urlencode(get_the_title());
+			echo '&amp;p[summary]=';
+			echo urlencode((wp_filter_nohtml_kses(apply_filters('the_content',get_the_excerpt()))));
+			if((function_exists('has_post_thumbnail')) && (has_post_thumbnail())) {
+				$my_thumb_id = get_post_thumbnail_id();
+				$my_thumb_array = wp_get_attachment_image_src($my_thumb_id);
+				$my_image = $my_thumb_array[0]; // this should be the url                
+				echo '&amp;p[images][0]=';
+				echo urlencode($my_image);
+			}
+			echo '&amp;p[url]=';
+			echo urlencode(get_permalink());
+			echo "','sharer','toolbar=0,status=0,width=626,height=436'); return false;\""; 
+			echo ' class="share" title="Send this to friends or post it on your profile.">Share This Post</a>';
+			echo '</span>';
+		} // end if for enable_share              
+		if($enable_external_link == "true"){
+			echo '<span class="wpbook_external_post"><a href="'. get_external_post_url(get_permalink()) .'"';
+			echo ' title="View this post outside Facebook at '. get_bloginfo('name') .'">View post on '. get_bloginfo('name') .'</a></span>';
+		}
+		echo '</p>';
+	} // end if for enable share, external, bottom
+	echo '</div>';	
+}
 echo '</div>';
 
                   
-  if ($give_credit == "true"){ 
+if ($give_credit == "true"){ 
 	echo '<div class="box_head clearfix" style="padding: 5px 0 0 0;">';
 	echo '<p><small>This Facebook Application powered by '
 		.'<a href="http://www.wordpress.org/extend/plugins/wpbook/">'
